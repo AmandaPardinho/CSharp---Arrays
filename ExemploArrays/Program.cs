13c286f565dc91851702ac2e10b530159ecaf854
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-09 15:35:17 -0300

    Exemplo de array de inteiros

---
+++ ExemploArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿void TestaArrayInt()
+{
+    int[] idades = new int[5];
+
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+
+    Console.WriteLine($"Tamanho do array {idades.Length}");
+
+    int acumulador = 0;
+    for(int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"índice [{i}] = {idade}");
+        acumulador += idade;
+    }
+
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Média de idades = {media}");
+}
+
+TestaArrayInt();
+
+Console.ReadKey();
