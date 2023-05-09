ee7db70130325dfa2ca4840c4cc92d0afceb26c0
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-09 18:42:22 -0300

    Código com o cálculo de média com valores de um array

--- MediaAmostra/Program.cs
+++ MediaAmostra/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Array amostras = Array.CreateInstance(typeof(double), 5);
+
+amostras.SetValue(20.22, 0);
+amostras.SetValue(27.5, 1);
+amostras.SetValue(5.5, 2);
+amostras.SetValue(4.8, 3);
+amostras.SetValue(9.1, 4);
+
+double acumulador = 0;
+for (int i = 0; i < amostras.Length; i++)
+{
+    acumulador += (double)amostras.GetValue(i);
+}
+
+double media = acumulador / amostras.Length;
+Console.WriteLine($"Média = {String.Format("{0:0.000}", media)}");
+
+Console.ReadKey();
