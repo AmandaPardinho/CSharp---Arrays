5f3a8f30907bb31a7e4305ca03f243392ec55a41
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-09 16:38:23 -0300

    Código com o método que fará a busca por uma determinada palavra no array montado pelo usuário

--- BuscarPalavra/Program.cs
+++ BuscarPalavra/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void BuscarPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for(int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.Write($"Digite a {i + 1}a. palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+
+    Console.Write("Digite a palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+
+    foreach(string palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {busca}");
+            break;
+        }
+    }
+}
+
+BuscarPalavra();
