6e4f5c33216aa6016d0267cb3783e5cb63cede3e
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-10 12:36:08 -0300

    Criação de método que associa o índice no array com a conta criada

--- bytebankAtendimento/Program.cs
+++ bytebankAtendimento/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Boas-vindas ao ByteBank, Atendimento.");
+﻿using bytebankAtendimento.Modelos.Conta;
+
+Console.WriteLine("Boas-vindas ao ByteBank, Atendimento.");
+
+void TestaArrayContasCorrentes()
+{
+    ContaCorrente[] listaContas = new ContaCorrente[]
+    {
+        new ContaCorrente(874, "5679787-A"),
+        new ContaCorrente(874, "4456668-B"),
+        new ContaCorrente(874, "7781438-C")
+    };
+
+    for (int i = 0; i < listaContas.Length; i++)
+    {
+        ContaCorrente contaAtual = listaContas[i];
+        Console.WriteLine($"Índice: {i} - Conta: {contaAtual.Conta}");
+    }
+}
+
+TestaArrayContasCorrentes();
+
+Console.ReadKey();
