5a6569aee0283a4dae590c06fc9ae5429c5427f3
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-24 18:48:16 -0300

    Desafio

---
+++ Nomes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> nomes = new List<string>()
+            {
+                "Bruce Wayne",
+                "Carlos Vilagran",
+                "Richard Grayson",
+                "Bob Kane",
+                "Will Farrel",
+                "Lois Lane",
+                "General Welling",
+                "Perla Letícia",
+                "Uxas",
+                "Diana Prince",
+                "Elisabeth Romanova",
+                "Anakin Wayne"
+            };
+
+            string nomeBusca = "Anakin Wayne";
+            bool encontrado = nomes.Contains(nomeBusca);
+
+            if (encontrado)
+            {                 
+                Console.WriteLine($"A pessoa {nomeBusca} foi encontrada");                
+            }
+            else
+            {
+                Console.WriteLine("Pessoa não encontrada");
+            }
+        }
+
+        //public bool BucaNome(List<string> lista, string busca)
+        //{
+        //    return lista.Contains(busca);
+        //}
+    }
+}
